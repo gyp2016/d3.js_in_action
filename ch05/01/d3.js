db08e37f9d6c0b00953a23824cf89c796307f8a5
GimYoungPhil <youngphil.gim@gmail.com>
2016-11-25 13:28:35 +0900

    fixed ch05/01

--- ch05/01/d3.js
+++ ch05/01/d3.js
@@ -14,9 +14,40 @@ function histogram(tweetsData) {
   let xAxis = d3.axisBottom(xScale)
                 .ticks(5);
 
-  let historyChart = d3.histogram();
+  let historyChartData = d3.histogram()
+                           .domain(xScale.domain())
+                           .value(function(d) {
+                             return d.favorites.length;
+                           })(tweetsData);
 
-  let histoData = historyChart(tweetsData);
+  d3.select('svg')
+    .selectAll('rect')
+    .data(historyChartData)
+    .enter()
+    .append('rect')
+    .attr('x', function(d) {
+      return xScale(d.x0);
+    })
+    .attr('y', function(d) {
+      return yScale(d.length);
+    })
+    .attr('width', function() {
+      return xScale(historyChartData[0].x1) - 2;
+    })
+    .attr('height', function(d) {
+      return 400 - yScale(d.length);
+    })
+    .style('fill', 'darkred')
+    .style('stroke', 'black')
+    .style('stroke-width', '1px');
 
-  console.log(histoData);
+  d3.select('svg')
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,400)')
+    .call(xAxis);
+
+  d3.select('g.axis')
+    .selectAll('text')
+    .attr('dx', 50);
 }
