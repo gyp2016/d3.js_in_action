559ce7dd03a81ff9ee33afff721caaa2a05e2b97
GimYoungPhil <youngphil.gim@gmail.com>
2016-11-26 22:32:49 +0900

    add ch05/05

--- ch05/05/d3.js
+++ ch05/05/d3.js
@@ -1,24 +1,27 @@
 let svg = d3.select('svg'),
     width = +svg.attr('width'),
     height = +svg.attr('height'),
-    g = svg.append('g').attr('transform', 'translate(40,0)');
-
-let cluster = d3.cluster()
-             .size([height, width - 160]);
+    g = svg.append('g')
+           .attr('transform', 'translate(' + (width / 2 + 40) + ',' + (height / 2 + 90) + ')');
 
 let stratify = d3.stratify()
                  .parentId(function(d) {
                    return d.id.substring(0, d.id.lastIndexOf('.'));
                  });
 
+let tree = d3.tree()
+             .size([360, 500])
+             .separation(function(a, b) {
+               return (a.parent == b.parent ? 1 : 2) / a.depth;
+             });
+
+  console.log(tree.size());
+
+
 d3.csv('flare.csv', function(error, data) {
   if (error) throw error;
 
-  let root = stratify(data).sort(function(a, b) {
-               return (a.height - b.height) || a.id.localeCompare(b.id);
-             });
-
-  cluster(root);
+  let root = tree(stratify(data));
 
   let link = g.selectAll('.link')
               .data(root.descendants().slice(1))
@@ -26,10 +29,10 @@ d3.csv('flare.csv', function(error, data) {
               .append('path')
               .attr('class', 'link')
               .attr('d', function(d, index) {
-                return 'M' + d.y + ',' + d.x +
-                       'C' + (d.parent.y + 100) + ',' + d.x +
-                       ' ' + (d.parent.y + 100) + ',' + d.parent.x +
-                       ' ' + d.parent.y + ',' + d.parent.x;
+                return 'M' + project(d.x, d.y) +
+                       'C' + project(d.x, (d.y + d.parent.y) / 2) +
+                       ' ' + project(d.parent.x, (d.y + d.parent.y) / 2) +
+                       ' ' + project(d.parent.x, d.parent.y);
               });
 
   let node = g.selectAll('.node')
@@ -40,22 +43,30 @@ d3.csv('flare.csv', function(error, data) {
                 return 'node' + (d.children ? ' node--internal' : ' node--leaf');
               })
               .attr('transform', function(d) {
-                return 'translate(' + d.y + ',' + d.x + ')';
+                return 'translate(' + project(d.x, d.y) + ')';
               });
 
   node.append('circle')
       .attr('r', 3);
 
   node.append('text')
-      .attr('dy', 3)
+      .attr('dy', '.31em')
       .attr('x', function(d) {
-        return d.children ? -8 : 8;
+        return d.x < 180 === !d.children ? 6 : -6;
       })
       .style("text-anchor", function(d) {
-        return d.children ? "end" : "start";
+        return d.x < 180 === !d.children ? 'start' : 'end';
+      })
+      .attr('transform', function(d) {
+        return 'rotate(' + (d.x < 180 ? d.x - 90 : d.x + 90) + ')'
       })
       .text(function(d) {
         return d.id.substring(d.id.lastIndexOf('.') + 1);
       });
 
+  function project(x, y) {
+    let angle = (x - 90) / 180 * Math.PI,
+        radius = y;
+    return [radius * Math.cos(angle), radius * Math.sin(angle)];
+  }
 });
