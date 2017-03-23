53f6a94f485a0ef0e6755161ea32dcebdf869a46
GimYoungPhil <youngphil.gim@gmail.com>
2017-03-24 02:41:43 +0900

    add ch07/02

--- ch07/02/main.js
+++ ch07/02/main.js
@@ -1,10 +1,14 @@
 d3.json("asia.geojson", createMap);
 
 function createMap(countries) {
-  
-  var aProjection = d3.geoMercator();
-  var geoPath = d3.geoPath(aProjection);
 
+  var width = 1600;
+  var height = 900;
+
+  var aProjection = d3.geoMercator()
+                      .scale(700)
+                      .translate([width / 2 - 1600, height / 2 + 400]);
+  var geoPath = d3.geoPath(aProjection);
   d3.select("svg")
     .selectAll("path")
     .data(countries.features)
